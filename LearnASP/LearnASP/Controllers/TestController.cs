9da9fe8524d962acc2c79d51abd3f09f350f44a4
daniel2992 <danieldemelash2992@gmail.com>
2021-06-01 03:11:03 -0700

    18

---
+++ LearnASP/LearnASP/Controllers/TestController.cs
@@ -0,0 +1,39 @@
+﻿using LearnASP.Models;
+using LearnASP.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnASP.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly TestRepository testRepository;
+
+        public TestController()
+        {
+            testRepository = new TestRepository();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public List<TestModel> T()
+        {
+            return testRepository.TestAllList();
+        }
+
+        public TestModel Twid(int id)
+        {
+            return testRepository.TestById(id);
+        }
+
+        public List<TestModel> Tsearch(string title,string author)
+        {
+            return testRepository.TestWithSearch(title,author);
+        }
+    }
+}
---
+++ LearnASP/LearnASP/Repository/TestRepository.cs
@@ -0,0 +1,36 @@
+﻿using LearnASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnASP.Repository
+{
+    public class TestRepository
+    {
+        public List<TestModel> TestAllList()
+        {
+            return DataSource();
+        }
+
+        public TestModel TestById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public List<TestModel> TestWithSearch(string title, string author)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) && x.Author.Contains(author)).ToList();
+        }
+
+        private List<TestModel> DataSource()
+        {
+            return new List<TestModel>()
+            {
+                new TestModel() { Id=1, Title="MVC", Author="Dani"},
+                new TestModel() { Id=2, Title="Razor", Author="da"},
+                new TestModel() { Id=1, Title="ASP", Author="Daniel"}
+            };
+        }
+    }
+}
